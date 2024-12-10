9adbfd7493bc5657cb430b538cbe6071b271fb13
mrspidy <74917158+MrxAravind@users.noreply.github.com>
2024-12-10 08:42:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from pyrogram import Client
 from scraper import *
 from config import *
 from database import *
-
-
+import asyncio
+from techzdl import TechZDL
 
 database_name = "Spidydb"
 db = connect_to_mongodb(DATABASE, database_name)
@@ -15,32 +15,30 @@ app = Client("Massify", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN, w
 
 
 
-def print_structure(data):
-    for item in data:
-        print(f"URL: {item.get('url')}")
-        print("Songs:")
-        for song in item.get("songs", []):
-            print(f"  - Name: {song.get('name')}")
-            print(f"    Song Link: {song.get('song_link')}")
-            print("    Download Links:")
-            for download in song.get("download_links", []):
-                print(f"      Quality: {download.get('quality')}")
-                print(f"      URL: {download.get('url')}")
-        print("Movie Info:")
-        movie_info = item.get("movie_info", {})
-        for key, value in movie_info.items():
-            print(f"  {key}: {value}")
-        print("=" * 40)
-
-
 
 async def main():
     async with app:
-        if check_db(db, collection_name, url):
-            data = fetch_main()
-        print_structure(data)
-        await app.send_audio(LOG_ID, audio=local_path)
-        insert_document(db, collection_name, result)
+        data = fetch_main()
+        for item in data:
+           print(f"URL: {item.get('url')}")
+           if check_db(db, collection_name, item.get('url')):
+              for song in item.get("songs", []):
+                 for download in song.get("download_links", []):
+                      print(f"{song.get('name')} - Quality: {download.get('quality')} - {song.get('song_link')}")
+                      print("Downloading....")
+                      downloader = TechZDL(url=download.get('url'))
+                      file_info = await downloader.get_file_info()
+                      file_name = file_info['filename']
+                      print(f"Filename: {file_info['filename']}")
+                      print(f"Total Size: {file_info['total_size']} bytes")
+                      await downloader.start()
+                      caption = "Metadata: \n"
+                      movie_info = item.get("movie_info", {})
+                      for key, value in movie_info.items():
+                           caption+= f"{key}: {value}\n"
+                      await app.send_audio(DUMP_ID,audio=file_name,caption=caption)
+                      result = item
+                      insert_document(db, collection_name, result)
                         
 
 
@@ -48,5 +46,5 @@ async def main():
 
 
 if __name__ == "__main__":
-    logging.info("Bot Started...")
+    logging.info("Music Bot Started...")
     app.run(main())
